8b3399b4c7fb8023c13a12c1e87aa2b5bba47d11
Alex00019 <52690454+Alex00019@users.noreply.github.com>
2019-07-09 12:51:59 +0600

    Summary_2

--- Kalkulator_easy/Kalkulator_easy/Form1.cs
+++ Kalkulator_easy/Kalkulator_easy/Form1.cs
@@ -16,5 +16,61 @@ namespace Kalkulator_easy
         {
             InitializeComponent();
         }
+
+        public static double summa(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double raznost(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double umnogenie(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double delenie(double a, double b)
+        {
+            return a / b;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)   {   }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)   {   }
+
+        private void label1_Click(object sender, EventArgs e)  {   }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(summa(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text)));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(raznost(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text)));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(umnogenie(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text)));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(delenie(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text)));
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
