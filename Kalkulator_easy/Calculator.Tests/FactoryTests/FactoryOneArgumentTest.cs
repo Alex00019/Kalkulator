01026e90aa5e11a941cf58021c203e331709e474
energy1storm <zhuravskii07@gmail.com>
2019-07-11 13:28:19 +0600

    Create Factory One Argument Test

---
+++ Kalkulator_easy/Calculator.Tests/FactoryTests/FactoryOneArgumentTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests.FactoryTests
+{
+    public class FactoryOneArgumentTest
+    {
+        [TestCase("Root", typeof(Root))]
+        [TestCase("Degree", typeof(Degree))]
+        [TestCase("sine", typeof(Sine))]
+        [TestCase("tan", typeof(Tangent))]
+        [TestCase("arcsine", typeof(Arcsine))]
+        [TestCase("arctan", typeof(Arctan))]
+        [TestCase("oneDivideByX", typeof(OneDivideByX))]
+        [TestCase("cose", typeof(Cose))]
+        [TestCase("exp", typeof(Exp))]
+        [TestCase("arccose", typeof(Arcose))]
+        [TestCase("arcctg", typeof(Arcctg))]
+        [TestCase("catangent", typeof(Catangent))]
+        [TestCase("logarithmTen", typeof(LogarithmTen))]
+        [TestCase("twoDivide", typeof(TwoDivideX))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = FactoryOneArgument.CreateCalculate(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
