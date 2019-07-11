405e341b2729c99d8bdf7e2aee1868426b4a675d
Alex00019 <52690454+Alex00019@users.noreply.github.com>
2019-07-11 11:22:27 +0600

    TestCase function Min and Max

---
+++ Kalkulator_easy/Calculator.Tests/TwoArguments/MaxTestCase.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Tests.TwoArguments
+{
+    public class MaxTestCase
+    {
+        [TestCase(0, 0, 0)]
+        [TestCase(3, 4, 4)]
+        [TestCase(-7, -2, -2)]
+        public void CalculateTest(
+    double firstValue,
+    double secondValue,
+    double expected)
+        {
+            var calculator = new Max();
+            var actualResult = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
+
+
---
+++ Kalkulator_easy/Calculator.Tests/TwoArguments/MinTestCase.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Tests.TwoArguments
+{
+    public class MinTestCase
+    {
+        [TestCase(5, 2, 2)]
+        [TestCase(10, 15, 10)]
+        [TestCase(-6.5, -9.24, -9.24)]
+        public void CalculateTest(
+    double firstValue,
+    double secondValue,
+    double expected)
+        {
+            var calculator = new Min();
+            var actualResult = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
+
